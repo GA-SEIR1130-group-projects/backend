0040544119b986911f7f5b353f62df37a8db1e82
biprajit163 <biprajit163@gmail.com>
2021-02-05 00:05:57 -0500

    add cors to index.js

--- app-backend/index.js
+++ app-backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express") 
-const app = express()  
+const app = express();
+const cors = require("cors");
 const router = require("./controllers/users.js");
 
 app.use(express.json()); 
 app.use(express.urlencoded({ entended: true })); 
+app.use(cors());
 
 app.set("port", process.env.PORT || 2000); 
 
